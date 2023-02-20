692e3230609b4dca4524f819c75b1a868b1ad9df
StartCodeingWithSayam <ssayam200@gmail.com>
2023-02-20 20:40:36 +0530

    First Commit

---
+++ app/src/main/java/com/flexstudio/musicon/fragments/PlaylistFragment.kt
@@ -0,0 +1,24 @@
+package com.flexstudio.musicon.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.flexstudio.musicon.R
+import com.flexstudio.musicon.databinding.FragmentPlaylistBinding
+
+class PlaylistFragment : Fragment() {
+    private lateinit var binding: FragmentPlaylistBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPlaylistBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
